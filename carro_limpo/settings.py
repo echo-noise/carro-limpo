73667e6f727caabf79bcac64cd6c604d75388ebe
Samara David <samara.david@outlook.com>
2021-12-09 21:56:00 -0300

    settings

--- carro_limpo/settings.py
+++ carro_limpo/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
