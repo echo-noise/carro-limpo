6fb96269120de2a1e5f7a112c7a07c95a86d880f
Samara David <samara.david@outlook.com>
2025-03-31 20:59:54 -0300

    fixes

--- carro_limpo/settings.py
+++ carro_limpo/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = getenv("IS_DEBUG", False)
+DEBUG = getenv("IS_DEBUG", True)
 
 ALLOWED_HOSTS = [
         getenv("APP_HOST")
